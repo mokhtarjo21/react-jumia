6daff3f621eb51b95b6032f7cc8706e19dd48d40
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-05-17 17:29:27 +0300

    solve problem

---
+++ src/components/login/HomeLogin.jsx
@@ -0,0 +1,36 @@
+
+import React, { useState,useContext, useEffect } from "react";
+import LoginForm from "./Loginpass";
+import Login from "./Login";
+import Active from "./active";
+import { instance } from "../../axiosInstance/instance";
+import { UserProvider } from '../../Context/user';
+import PersonalDetailsForm from "./userInfo2";
+import AddPassword from "./userInfo1";
+import AddNameAndPhone from "./userInfo3";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function JumiaLogin() {
+  
+
+  return (
+    <>
+  
+
+
+<UserProvider>
+    
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="login" element={<LoginForm  />}/>
+                    <Route path="active" element={<Active />}/>
+                    <Route path='info1' element={<AddPassword />}/>
+                    <Route path='info2' element={<PersonalDetailsForm />}/>
+                    <Route path='info3' element={<AddNameAndPhone />}/>
+                </Routes>
+              
+    </UserProvider>
+           
+    </>
+  );
+}
+export default JumiaLogin
