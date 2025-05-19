7177fc96ebc14bdbf7d5b82e2c732a214619ee75
Amr Hussain <amrohossnei@gmail.com>
2025-05-20 00:16:57 +0300

    aded browse by category div in the category page, with prototypes of browse by brand, bread crumb navigator

--- src/components/category_grid/category_card.jsx
+++ src/components/category_grid/category_card.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import "./category_card.css";
 const CategoryCard = ({ title, imageSrc, href }) => {
   return (
-    <Link to={href} className="link-wrapper">
+      <Link to={`/${href}`} className="link-wrapper">
       <Card className="card-container">
         <Card.Img  src={imageSrc} className="image-div" />
         <Card.Body className="CardTitle">
--- src/components/category_grid/category_grid.jsx
+++ src/components/category_grid/category_grid.jsx
@@ -1,6 +1,6 @@
 import CategoryCard from "./category_card";
 import PropTypes from "prop-types";
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./category_grid.css";
 
 // for arrow icon
@@ -9,8 +9,25 @@ import { MdChevronLeft } from "react-icons/md";
 
 const SCROLL_AMOUNT = 250;
 
-const CategoryGrid = ({ categories }) => {
+const CategoryGrid = ({ categories, header }) => {
   const scrollRef = useRef(null);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
+  const updateArrows = () => {
+    if (scrollRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+      setCanScrollLeft(scrollLeft > 0);
+      setCanScrollRight(scrollLeft + clientWidth < scrollWidth);
+    }
+  };
+
+  useEffect(() => {
+    updateArrows();
+    const handleResize = () => updateArrows();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const scrollLeft = () => {
     if (scrollRef.current) {
@@ -27,27 +44,25 @@ const CategoryGrid = ({ categories }) => {
   return (
     <div className="category-grid-outer">
       <div className="category-grid-header">
-        <span>Shop By Category...</span>
+        <span>Shop By {header}...</span>
       </div>
-      <div
-        className="category-grid-arrow category-grid-arrow-left"
-        onClick={scrollLeft}
-      >
-        <MdChevronLeft size={20} color="white" />
-      </div>
-      <div
-        className="category-grid-arrow category-grid-arrow-right"
-        onClick={scrollRight}
-      >
-        <MdChevronRight size={20} color="white" />
-      </div>
-      <div className="category-grid-scroll" ref={scrollRef}>
+      {canScrollLeft && (
+        <div className="category-grid-arrow category-grid-arrow-left" onClick={scrollLeft}>
+          <MdChevronLeft size={20} color="white" />
+        </div>
+      )}
+      {canScrollRight && (
+        <div className="category-grid-arrow category-grid-arrow-right" onClick={scrollRight}>
+          <MdChevronRight size={20} color="white" />
+        </div>
+      )}
+      <div className="category-grid-scroll" ref={scrollRef} onScroll={updateArrows}>
         {categories.map((category) => (
           <CategoryCard
             key={category.id}
-            title={category.title}
-            imageSrc={category.imageSrc}
-            href={category.href}
+            title={category.name}
+            imageSrc={category.image}
+            href={category.slug}
           />
         ))}
       </div>
