660e383d663c891ff64c2faa208ad1c77a7829bf
Amr Hussain <amrohossnei@gmail.com>
2025-05-20 11:55:24 +0300

    renamed profile.css to profile.module.css and used import style from 'xyz'

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -3,13 +3,13 @@ import Sidebar from './Sidebar';
 import OverviewSection from './OverviewSection';
 import SuggestedProducts from './SuggestedProducts';
 import RecentlyViewed from './RecentlyViewed';
-import './profile.css';
+import styles from './profile.module.css';
 
 const ProfilePage = () => {
   return (
-    <div className="profile-wrapper">
+    <div className={styles['profile-wrapper']}>
       <Sidebar />
-      <div className="profile-content">
+      <div className={styles['profile-content']}>
         <OverviewSection />
         <SuggestedProducts />
         <RecentlyViewed />
