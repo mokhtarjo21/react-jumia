9e9685514edabf2b739d8f2d8875ff972b659970
Amr Hussain <amrohossnei@gmail.com>
2025-05-23 09:24:59 +0300

    enhanced header by making it fully responsive to screen sizes and redirects

--- src/components/home/home_header_component/category_tree_component/subcategory_panel.jsx
+++ src/components/home/home_header_component/category_tree_component/subcategory_panel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './category_menu.module.css';
-
+import { Link } from 'react-router-dom';
 const SubcategoryPanel = ({ category, onHover, onLeave }) => {
   if (!category || !category.children || category.children.length === 0) return null;
   
@@ -13,11 +13,11 @@ const SubcategoryPanel = ({ category, onHover, onLeave }) => {
       <div className={styles.flexContainer}>
         {category.children.map(sub => (
           <div className={styles.subcategoryGroup} key={sub.id}>
-            <div className={styles.subcategoryGroupTitle}>{sub.name}</div>
+            <Link to={`/${sub.slug}`} className={styles.subcategoryGroupTitle}>{sub.name}</Link>
             <div className={styles.subcategoryList}>
               {sub.children && sub.children.length > 0 ? (
                 sub.children.map(child => (
-                  <a href={`/category/${child.slug}`} key={child.id}>{child.name}</a>
+                  <Link  className={styles._3rdLevel} to={`/${child.slug}`} key={child.id}>{child.name}</Link>
                 ))
               ) : (
                 <span style={{ color: '#aaa', fontSize: '0.9em' }}>No subcategories</span>
